5ec5223c9dd25603be348b07e8f73dad50cdd80f
greedybrain <greedybrain18@gmail.com>
2020-12-13 15:43:39 -0500

    ...

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Header.css'
+
+class Header extends Component {
+        render() {
+                return (
+                        <header className="header">
+                                {/* LOGO  ON LEFT SIDE */}
+                        </header>
+                );
+        }
+}
+
+export default Header;
